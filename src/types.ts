68ca61f740914f89fec82c70ece9626a36dba1b3
danifezi <ifezued@gmail.com>
2025-07-16 01:42:23 +0100

    nn

---
+++ src/types.ts
@@ -0,0 +1,39 @@
+
+export interface BudgetAnalysis {
+    userBudget: number;
+    estimatedCost: number;
+    difference: number;
+    summary: string;
+}
+
+export interface BudgetItem {
+    itemName: string;
+    quantity: string;
+    estimatedPrice: number;
+}
+
+export interface PriceAnalysisItem {
+    itemName: string;
+    priceStability: string;
+    savingTip: string;
+}
+
+export interface Merchant {
+    name: string;
+    address: string;
+    deals: string;
+}
+
+export interface ShoppingPlan {
+    budgetAnalysis: BudgetAnalysis;
+    budgetItems: BudgetItem[];
+    priceAnalysis: PriceAnalysisItem[];
+    recommendedMerchants: Merchant[];
+}
+
+export interface AdCopy {
+    headline: string;
+    body: string;
+    callToAction: string;
+    hashtags: string[];
+}
