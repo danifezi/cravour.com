030d8b6814acc6f8a21bda83c87e9650b5115cd0
danifezi <ifezued@gmail.com>
2025-07-16 18:18:33 +0100

    w

--- index.tsx
+++
@@ -1,2 +0,0 @@
-
-// This file is obsolete. The main entry point for the application is /src/index.tsx and is processed by Webpack.
--- script.js
+++
@@ -1,2 +0,0 @@
-// This file is intentionally left blank to resolve a syntax error.
-// All necessary landing page scripts are handled in index.tsx.
