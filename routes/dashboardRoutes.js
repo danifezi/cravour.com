94c0e1b5d49b7169095eb6d0da0c7035edff40eb
danifezi <ifezued@gmail.com>
2025-07-19 20:34:35 +0100

    load

--- routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
-const authenticateToken = require('../middleware/authMiddleware');
-
-router.post('/', authenticateToken, dashboardController.generateDashboard);
-
-module.exports = router;
--- routes/waitlistRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const waitlistController = require('../controllers/waitlistController');
-const validate = require('../middleware/validationMiddleware');
-const schemas = require('../src/config/schemas');
-
-router.post('/join', validate(schemas.waitlistSchema), waitlistController.joinWaitlist);
-
-module.exports = router;
